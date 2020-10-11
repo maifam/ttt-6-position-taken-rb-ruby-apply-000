6cab2732a47a0330632afa3c7f80c8cc03ac8e42
Mai Pham <maitphm@gmail.com>
2020-10-11 03:06:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
-def position_taken(board, index)
+def position_taken?(board, index)
   if (board[index] ==  " ") || (board[index] == "") || (board[index] == nil)
     taken = false
   else
     taken = true 
   end
-taken 
+  taken 
 end 
